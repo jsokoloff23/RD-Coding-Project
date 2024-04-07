07d92f16256ad464058858d14a21becfe3b923a9
jsokoloff23 <jonahs@uoregon.edu>
2024-04-06 18:06:34 -0700

    first commit

---
+++ problem_3_test.py
@@ -0,0 +1,41 @@
+"""
+File used to test functionality for problem 3
+"""
+
+import tifffile
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+from skimage.color import label2rgb
+
+
+#parameters
+filename = "Assoc_RDEng_test.tif"
+
+
+image_stack = tifffile.TiffFile(filename)
+image = image_stack.pages[0].asarray()
+
+# apply threshold
+thresh = threshold_otsu(image)
+# label image regions
+label_image = label(image > thresh)
+# to make the background transparent, pass the value of `bg_label`,
+# and leave `bg_color` as `None` and `kind` as `overlay`
+image_label_overlay = label2rgb(label_image, image=image)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.imshow(image_label_overlay)
+
+for region in regionprops(label_image):
+    #use regions in area range to remove objects that are too small to be
+    #cells and too large to be single cells.
+    if 100 <= region.area <= 2000:
+            # draw rectangle around segmented coins
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                fill=False, edgecolor='red', linewidth=2)
+        ax.add_patch(rect)
+plt.show()
