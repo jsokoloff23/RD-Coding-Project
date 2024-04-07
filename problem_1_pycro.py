7c8fa3eda05241ca73cfd8316bb2eb6fbf5f82fb
jsokoloff23 <jonahs@uoregon.edu>
2024-04-06 19:56:44 -0700

    added z_end_correction and formatting notes: pycro-manager takes the wrong number of slices in a z-stack, likely due to float precision. Correction is added to guarantee the correct number of slices are acquired.

--- problem_1_pycro.py
+++ problem_1_pycro.py
@@ -1,7 +1,10 @@
-import pycro
-import utils
+"""
+Problem 1 using Pycromanager Acquisition class to perform acquisition.
+"""
 from pycromanager import Acquisition, multi_d_acquisition_events
 
+import pycro
+import utils
 
 #user parameters
 save_dir = r"C:/cz/problem_1"
@@ -12,12 +15,12 @@ z_start = 0
 #set to 1 for positive and -1 for negative
 z_stack_direction = 1
 
-
 #other acquisition parameters
-#for some reason, this takes (z_slices - 1) takes 13 slices, so -2 instead
-z_end = z_start + z_stack_direction*(z_slices - 2)*step_size
+#for some reason, (z_slices - 1) takes 13 slices but (z_slices - 1) 
+#takes 11! z_end_correction is used as a correction to compensate for this.
+z_end_correction = -z_stack_direction*step_size/100
+z_end = z_start + z_stack_direction*(z_slices - 1)*step_size + z_end_correction
 xy_positions = utils.get_stage_grid_positions()
-
 
 with Acquisition(save_dir, "acquisition") as acq:
     events = multi_d_acquisition_events(z_start=z_start,
