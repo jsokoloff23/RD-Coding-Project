a92e7bd20ed4f7069be7d331a91304bbb9d9dd2f
jsokoloff23 <jonahs@uoregon.edu>
2024-04-06 19:02:07 -0700

    removed closing and updated labels

--- problem_3.py
+++ problem_3.py
@@ -3,30 +3,31 @@ import matplotlib.pyplot as plt
 
 from skimage.filters import threshold_otsu
 from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
-from skimage.color import label2rgb
 
 
 filename = "Assoc_RDEng_test.tif"
+hist_save_path = "problem 3 output/nucleus_eccentricity.png"
+
 
 image_stack = tifffile.TiffFile(filename)
 eccentricities = []
 for page_num, page in enumerate(image_stack.pages):
+    #labeling code adapted from this website:
+    #https://scikit-image.org/docs/stable/auto_examples/segmentation/plot_label.html
     image = page.asarray()
     #Image set is clearly bimodal (see testing), so otsu's seems reasonable
     thresh = threshold_otsu(image)
-    #close to get rid of small speckles and non-cell objects
-    thresholded = closing(image > thresh, square(3))
     #label image with thresholded
-    labeled = label(thresholded)
+    labeled = label(image > thresh)
     for region in regionprops(labeled):
         #use regions in area range to remove objects that are too small to be
         #cells and too large to be single cells.
         if 100 <= region.area <= 2000:
             eccentricities.append(region.eccentricity)
 
-plt.hist(eccentricities, 30, color="orange")
+
+plt.hist(eccentricities, 40, color="orange")
 plt.title("Cell Nucleus Eccentricities")
-plt.xlabel("Eccentricities")
+plt.xlabel("Ecentricity")
 plt.ylabel("Nucleus Count")
-plt.show()
+plt.savefig(hist_save_path)
